ba646d8ca278637de3c0da8e84e0519e9c395c43
Alex Pop <apop@chef.io>
2017-11-11 11:07:37 +0000

    try local storage

--- js/index.js
+++ js/index.js
@@ -12,13 +12,7 @@ var SignupForm = Vue.component('signup-form', {
       fulladdr_msg: '',
       loanamount: '1000',
       loanamount_msg: '',
-      password1: '',
-      pwd1_msg: '',
-      password2: '',
-      pwd2_msg: '',
-      disable_btn: true,
-      msg1: true,
-      msg2: true
+      disable_btn: true
     }
   },
 
@@ -57,53 +51,13 @@ var SignupForm = Vue.component('signup-form', {
         return false;
       }
     },
-    check_password_length(value, msg, total) {
-      var length = value.length;
-      var sum = 0;
-      var display;
-
-      sum = (total - length);
-
-      switch(sum) {
-        case 0:
-          display = '';
-          break;
-        case 1:
-          display = 'Keep going - just need '+ sum + ' more character.';
-          break;
-        default:
-          display = 'Keep going - just need '+ sum + ' more characters';
-      }
-
-      if(length >= total) {
-        this[msg] = '';
-        return true;
-      } else {
-        this[msg] = display;
-        return false;
-      }
-
-    },
-
-    check_passwords_match() {
-      if(this.password1.length > 5 && this.password2.length > 5) {
-          if(this.password2 != this.password1) {
-            this.pwd2_msg = 'Passwords do not match';
-            this.disable_btn = true;
-            return true;
-          } else {
-            this.pwd2_msg = '';
-            this.disable_btn = false;
-            return false;
-          }
-        }
-    },
-
     on_signup() {
+      console.log("In on_signup(), fullname="+this.fullname);
+      //debugger;
+      window.localStorage.setItem("fullname", this.fullname);
+
       this.fullname = '';
       this.fullname_msg = '';
-      this.msg1 = false;
-      this.msg2 = false;
       this.disable_btn = true;
       this.$emit('change_comp', 'results');
     },
@@ -120,9 +74,17 @@ var Results = Vue.component('results', {
   // TEMPLATE
   template: '#results',
 
+  // DATA
+  data() {
+    return {
+      punter: window.localStorage.getItem("fullname")
+    }
+  },
+
   // METHODS
   methods: {
     back_to_signup() {
+      console.log('In back_to_signup()');
       this.$emit('change_comp', 'signup-form');
     }
   }
@@ -149,10 +111,14 @@ new Vue({
   // ELEMENT
   el: '#app',
 
+
   // DATA
-  data: {
-    compname: 'signup-form'
+  data() {
+    return {
+      compname: 'signup-form'
+    }
   },
+
 
   // COMPONENTS
   components: {
