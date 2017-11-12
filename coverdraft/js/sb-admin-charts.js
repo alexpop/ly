518c001329a6089e5e3ff8c091ebfbb1d94854f3
influxious <yin.noe8@gmail.com>
2017-11-12 12:04:03 +0000

    Updated Pie Charts

--- coverdraft/js/sb-admin-charts.js
+++ coverdraft/js/sb-admin-charts.js
@@ -99,7 +99,7 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'pie',
   data: {
-    labels: ["Blue", "Red", "Yellow", "Green"],
+    labels: ["Transport", "Food & Beverage", "Entertainment", "Bills"],
     datasets: [{
       data: [12.21, 15.58, 11.25, 8.32],
       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
